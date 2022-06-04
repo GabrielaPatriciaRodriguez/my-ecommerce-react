bc511a662dff85e522261b4db0504412ae2fc5fa
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2022-06-04 15:58:18 -0300

    agregando menu navegable

---
+++ src/components/Navigation.js
@@ -0,0 +1,27 @@
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Hombre</a>
+        </li>
+        <li>
+          <a href="#">Mujer</a>
+        </li>
+        <li>
+          <a href="#">Niño</a>
+        </li>
+        <li>
+          <a href="#">Niña</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
