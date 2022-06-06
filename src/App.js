3ebed7aafb9e96608911a6c2d44c4a413db86d1a
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2022-06-06 18:44:21 -0300

    cambiando class por function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-
 import React from "react";
 
 import "./App.css";
 
 import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  render() {
+function App (){
     return (
       <div className="App">
         <Navigation />
-        <h1>Mi tienda Virtual</h1>
+        <h1>Mi tiendaVirtual</h1>
         <p>Gabriela P. Rodriguez</p>
       </div>
     );
-  }
 }
 
 export default App;
